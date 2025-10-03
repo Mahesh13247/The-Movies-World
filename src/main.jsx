8670002f9f596e8aeedc3305880bd3ef9df1d580
Mahesh13247 <139057471+Mahesh13247@users.noreply.github.com>
2025-10-03 19:45:49 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,28 +5,28 @@ import { AppProvider } from "./context/AppContext";
 import { config } from "./config";
 
 // Conditionally disable right-click and developer tools based on configuration
-if (config.security.enableDevToolsBlocking) {
-  document.addEventListener("contextmenu", (e) => e.preventDefault());
-}
+// if (config.security.enableDevToolsBlocking) {
+//   document.addEventListener("contextmenu", (e) => e.preventDefault());
+// }
 
-const ctrlShiftKey = (e, keyCode) => {
-  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-};
+// const ctrlShiftKey = (e, keyCode) => {
+//   return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+// };
 
-if (config.security.enableDevToolsBlocking) {
-  document.onkeydown = (e) => {
-    // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-    if (
-      e.keyCode === 123 ||
-      ctrlShiftKey(e, "I") ||
-      ctrlShiftKey(e, "J") ||
-      ctrlShiftKey(e, "C") ||
-      (e.ctrlKey && e.keyCode === "U".charCodeAt(0))
-    ) {
-      return false;
-    }
-  };
-}
+// if (config.security.enableDevToolsBlocking) {
+//   document.onkeydown = (e) => {
+//     // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+//     if (
+//       e.keyCode === 123 ||
+//       ctrlShiftKey(e, "I") ||
+//       ctrlShiftKey(e, "J") ||
+//       ctrlShiftKey(e, "C") ||
+//       (e.ctrlKey && e.keyCode === "U".charCodeAt(0))
+//     ) {
+//       return false;
+//     }
+//   };
+// }
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProvider>
